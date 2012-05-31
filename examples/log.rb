bb8b3db642436c69736ec223134670ea93d2a8dd
Andrew Aladjev <aladjev.andrew@gmail.com>
2012-06-01 01:14:21 +0300

    impl

--- examples/log.rb
+++ examples/log.rb
@@ -1,4 +1,28 @@
 require "./lib/log.rb"
 
+# not exist
+puts Ecc::Log.new(3, 7, 30).brute().inspect
+
+# # single answer - 9
 puts Ecc::Log.new(3, 14, 17).brute().inspect
+
+# multiple answers - [2, 4, 6, 8]
 puts Ecc::Log.new(2155, 1545, 20).brute().inspect
+
+# 6 is generator
+log = Ecc::Log.new(3, 4, 13)
+puts log.is_generator(6).to_s
+puts log.is_generator(10).to_s
+
+# random generator
+puts log.random_generator
+
+# baby-step giant-step algorithm. answer - 982
+puts Ecc::Log.new(2341, 1, 983).baby_step_giant_step
+
+# generate example in Zp
+log = Ecc::Log.new(nil, nil, 104729)
+log.a = log.random_generator
+log.b = rand(log.n - 1).floor
+puts "a = " + log.a.to_s + ", b = " + log.b.to_s + ", n = " + log.n.to_s
+puts "solution = " + log.baby_step_giant_step.to_s
--- lib/log.rb
+++ lib/log.rb
@@ -1,5 +1,7 @@
 module Ecc
   class Log
+    attr_accessor :a, :b, :n
+    
     @@primes = {}
     
     def initialize (a, b, n)
@@ -8,58 +10,123 @@ module Ecc
       @n      = n
       @fn     = Log.eiler n
     end
+    
+    def baby_step_giant_step
+      unless Log.is_prime @n and self.is_generator @a
+        puts "n is not prime or a is not a generator"
+        return nil
+      end
+      
+      h = Math.sqrt(@n).floor() + 1
+      c = (@a ** h) % @n
+      
+      list_1 = []
+      for i in (1..h)
+        list_1.push((c ** i) % @n)
+      end
+      
+      for j in (1..h)
+        element = (@b * (@a ** j)) % @n
+        i = list_1.index element
+        unless i.nil?
+          return h * (i + 1) - j
+        end
+      end
+      nil
+    end
+    
+    def polig_hellman
+      unless Log.is_prime @n and self.is_generator @a
+        puts "n is not prime or a is not a generator"
+        return nil
+      end
+      
+      factors = Log.factor @n - 1
+    end
       
     def brute
       res = []
-      for i in (1...@fn)
+      for i in (1..@fn)
         if ((@a ** i) - @b) % @n == 0
           res.push i
         end
       end
       res
     end
+    
+    # is a a generator of the cyclic group Zp?
+    def is_generator (a)
+      factors = Log.factor @n - 1
+      for factor in factors
+        if (a ** ((@n - 1) / factor[0])) % @n == 1
+          return false
+        end
+      end
+      true
+    end
+    
+    # get a random generator of the cyclic group Zp
+    def random_generator ()
+      a = Time.now.usec
+      while not self.is_generator(a)
+        a += 1
+      end
+      a
+    end
   
     def self.eiler (num)
       factors = Log.factor num
-      res = 1
+      res = num
       for factor in factors
-        res *= 1 - 1.0 / factor
+        res *= 1 - 1.0 / factor[0]
       end
-      num * res
+      res.floor
     end
   
     def self.factor (num)
       factors = []
       length = Math.sqrt(num).floor
-      if length < 2
-        return []
-      end
 
       for i in (2..length)
         if num % i == 0
           if Log.is_prime(i)
-            factors.push i
+            k = 1
+            num_copy = num / i
+            while num_copy % i == 0
+              num_copy /= i
+              k += 1
+            end
+            factors.push [i, k]
           end
           j = num / i
           if j != i
             if Log.is_prime(j)
-              factors.push j
+              k = 1
+              num_copy = num / j
+              while num_copy % j == 0
+                num_copy /= j
+                k += 1
+              end
+              factors.push [j, k]
             end
           end
         end
       end
-      
-      factors.sort{ |a, b|
-        a - b
-      }
+
+      if factors.length == 0
+        @@primes[num] = true
+        [[num, 1]]
+      else
+        @@primes[num] = false
+        factors
+      end
     end
   
     def self.is_prime (num)
-      if @@primes.has_key? num
-        @@primes[num]
-      else
-        @@primes[num] = Log.factor(num).length == 0
+      unless @@primes.has_key? num
+        Log.factor num
       end
+      @@primes[num]
     end
   end
 end
