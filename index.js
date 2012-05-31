4dcbd985e4ab0d30c8f46270f209b5992199ba1b
aladjev.andrew <aladjev.andrew@gmail.com>
2012-05-31 17:25:17 +0300

    impl

--- index.js
+++
@@ -1,9 +0,0 @@
-var curve = require("./lib/elliptic-curve"),
-    point = require("./lib/elliptic-point"),
-    diffieHellman = require("./lib/elliptic-diffie-hellman"),
-    elgamel =  require("./lib/elliptic-elgamel");
-
-exports.curve = curve;
-exports.point = point;
-exports.diffieHellman = diffieHellman;
-exports.elgamel = elgamel;
---
+++ source/javascripts/log.js.coffee
@@ -0,0 +1,5 @@
+#= require_tree ./lib
+#= require elliptic/log
+
+# console.log new Ecc.Log(3, 14, 17).brute()
+console.log new Ecc.Log(2155, 1545, 20).brute()
